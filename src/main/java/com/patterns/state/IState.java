a3470b0c26e28b8d2a7c80abaa1294cd85a1f195
Dmitry Denisenko <denisenko@estaxi.ru>
2020-08-05 10:28:03 +0600

    deleted unused class

--- src/main/java/com/patterns/state/IState.java
+++ src/main/java/com/patterns/state/IState.java
@@ -1,17 +1,11 @@
 package com.patterns.state;
 
 public interface IState {
-<<<<<<< HEAD
 
-    char getSymbol();
-
-    char changeSymbol();
-=======
     void checkSentPhone(CallRouting callRouting);
 
     RouteStateEnum getStatusState();
 
     String getDescriptionContext();
->>>>>>> state
 
 }
